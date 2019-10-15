e0be45139778bdfe9be94424b941a75afa1a5701
MackRoe <elaine.music@students.makeschool.com>
2019-10-15 16:14:40 -0700

    Added MongoDB and profiles model

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
 
 app = Flask(__name__)
+client = MongoClient()
+db = client.InstaShout
+profiles = db.profiles
 
 @app.route('/')
 def index():
     """Return homepage."""
     return render_template('home.html', msg="🔊 InstaShout 🔊")
+
+""" USER STORIES
+Users can create a profile (new/create)
+Users can view their profile (show)
+Users can delete their profile (destroy)
+Users can edit their profile (edit/update)
+User can send an InstaShout
+User is informed their InstaShout has been sent
+"""
 
 if __name__ == '__main__':
     app.run(debug=True)
