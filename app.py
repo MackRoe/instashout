dd858e921dbc2516802fb86895b1f3977af3fcc8
MackRoe <elaine.music@students.makeschool.com>
2019-10-15 20:02:07 -0700

    added sign up form

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 client = MongoClient()
@@ -19,6 +20,31 @@ Users can edit their profile (edit/update)
 User can send an InstaShout
 User is informed their InstaShout has been sent
 """
+
+@app.route('/profile/new')
+def profile_new():
+    """Create a new profile"""
+    return render_template('profile_new.html')
+
+@app.route('/profile/view', methods=['POST'])
+def profile_submit():
+    """Submit a new playlist."""
+    profile = {
+        'profile_name': request.form.get('profile_name'),
+        'profile_number': request.form.get('profile_number'),
+        'contact_number': request.form.get('contact_number'),
+        'contact_provider': request.form.get('contact_provider')
+    }
+    # add info to db with the following:
+    profiles.insert_one(profile)
+    return redirect(url_for('profile_view'))
+
+@app.route('/profile/<profile_id>')
+def profile_view(profile_id):
+    """Show profile"""
+    # performs GET from db
+    profile = profiles.find_one({'_id': ObjectId(profile_id)})
+    return render_template('profile_view.html', profile=profile)
 
 if __name__ == '__main__':
     app.run(debug=True)
